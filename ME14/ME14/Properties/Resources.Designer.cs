0b29528ad90d791515a0f571906b59592ee85f6d
hlii0085 <hlii0085@student.monash.edu>
2020-08-31 14:05:24 +1000

    page output

--- ME14/ME14/Properties/Resources.Designer.cs
+++ ME14/ME14/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace ME14.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap homepage {
+            get {
+                object obj = ResourceManager.GetObject("homepage", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap know_you_better {
             get {
                 object obj = ResourceManager.GetObject("know_you_better", resourceCulture);
