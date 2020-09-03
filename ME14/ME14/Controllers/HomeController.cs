17e2b9ac0a267229f329d164ec32fc7b39f59935
nfuu0001 <nfuu0001@student.monash.edu>
2020-09-03 23:48:40 +0800

    comments

--- ME14/ME14/Controllers/HomeController.cs
+++ ME14/ME14/Controllers/HomeController.cs
@@ -8,11 +8,13 @@ namespace ME14.Controllers
 {
     public class HomeController : Controller
     {
+        //control the index page
         public ActionResult Index()
         {
             return View();
         }
 
+        //control the About page
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,12 +22,15 @@ namespace ME14.Controllers
             return View();
         }
 
+        //control the Contact page
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
+        //control the info page. Info is added by me
         public ActionResult Info()
         {
             ViewBag.Message = "Your Info page.";
