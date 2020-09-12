f012226ec081c453f3b5e8d99bcebbdb162219cb
hlii0085 <hlii0085@student.monash.edu>
2020-09-12 19:28:29 +1000

    iteration2_start

---
+++ ME14/ME14/obj/Release/Package/PackageTmp/Scripts/password.js
@@ -0,0 +1,9 @@
+﻿var password = "";
+password = prompt('Please Enter the Password(Only If You Enter the Correct Password):', '');
+
+while (password != '334455') {
+    alert("Wrong Password");
+    password = prompt('Please Enter the Password(Only If You Enter the Correct Password):', '');
+
+    //window.opener = null; window.close();
+}
